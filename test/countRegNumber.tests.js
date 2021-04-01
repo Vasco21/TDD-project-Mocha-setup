347b28a3e242c54719a5179bcf63885cece6692c
Vasco Eti <vascoeti123@gmail.com>
2021-04-01 11:21:39 +0200

    upadate

--- test/countRegNumber.tests.js
+++ test/countRegNumber.tests.js
@@ -5,7 +5,7 @@ describe('The countRegNumber function', function(){
     });
     it('should return the total of the regNo on the list.', function(){
         
-        assert.equal(1, countRegNumber("CA 182736","CY 523519","CJ 812328"));
+        assert.equal(3, countRegNumber("CA 182736 ,CY 523519, CJ 812328"));
     });
     
 });
